20bf02a57be046c3cebd2812da5163be970ceb1c
Gabriel METZGER <gab.metzger@gmail.com>
2014-08-06 14:45:23 +0200

    add form popups

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+  //Don't display popup form on mobile devices
+	if(Response.deviceW() >= 961) {
+		$('#demoModal').modal({
+      		show: false
+    	});
+    	$('#suscribeModal').modal({
+      		show: false
+    	});
+	}
+	else {
+		$('.btn-demo').click(function() {
+			$(location).attr('href',"formDemo.html");
+		});
+    $('.btn-suscribe').click(function() {
+      $(location).attr('href',"formSuscribe.html");
+    });
+	}
+    
+});
